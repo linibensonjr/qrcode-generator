92095cce52afeb874470e433c851f2fecdb95b18
linibensonjr <linibensonjr@gmail.com>
2023-02-20 09:05:23 +0100

    feat: Update model to support imagefield

--- qrcode/genqrcode/models.py
+++ qrcode/genqrcode/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class QRCode(models.Model):
     text = models.TextField(null=False, help_text="Enter text to generate")
     qr_name = models.TextField(null=False, help_text="Enter qrcode name")
-
+   
     def __str__(self):
         return self.qr_name
